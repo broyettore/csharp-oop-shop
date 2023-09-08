3b1951f2a92a90be905c99dd7df6456ac32f54a9
broyettore <broyettore@gmail.com>
2023-09-08 17:22:32 +0200

    Exercise done with bonus

---
+++ csharp-oop-shop/Product.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    public class Product
+    {
+        // ATTRIBUTES
+        public int Code { get; } // Read only
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public int VAT { get; private set; } = 22; // VAT fixed at 22%
+
+
+        // BASIC CONSTRUCT
+        public Product(string name, string description, int price)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.Price = price;
+
+            // random code generated
+            this.Code = GenerateRand();
+        }
+
+
+        // METHOD TO GET PRICE + VAT
+        public int GetPriceWithVat()
+        {
+            int ResultVat = (this.Price * this.VAT) / 100;
+            return this.Price + ResultVat;
+        }
+
+
+        // METHOD TO GET CODE + NAME
+        public string GetCodeName()
+        {
+            return this.Code + "-" + this.Name;
+        }
+
+
+        // METHOD TO GENERATE A RANDOM NUM, RANGE 1 - 200
+        public int GenerateRand()
+        {
+            int min = 1;
+            int max = 100;
+            Random rand = new Random();
+            int randNum = rand.Next(min, max);
+            return randNum;
+        }
+        
+    }
+}
+
+
+//VAT = (BASE PRICE * VAT) / 100
--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -1,10 +1,54 @@
-﻿namespace csharp_oop_shop
+﻿using System.Globalization;
+using System.Text;
+
+namespace csharp_oop_shop
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // UTF8 reading in console
+            Console.OutputEncoding = Encoding.UTF8;
+
+            // function to add left padding to a number to reach 8 total digits
+            string AddPadToNum(int number)
+            {
+                string padNum = number.ToString().PadLeft(8, '0');
+
+                if (padNum.Length > 8)
+                {
+                    padNum = padNum.Substring(0, 8);
+                }
+
+                return padNum;
+            }
+
+            // Products
+            Product product1 = new("RT-BassX", "Sony Wireless Headphones", 80);
+            Product product2 = new("SmartWatch", "Apple Smartwatch", 213);
+            Product product3 = new("MousePad", "RGB Light Mousepad", 24);
+            Product product4 = new("MacBook Air", "Apple Laptop with good specs", 1200);
+            Product product5 = new("RTX-4070", "Graphic Card for gamers", 2200);
+
+
+            // Products Array
+            Product[] productList = { product1, product2, product3, product4, product5 };
+
+            for (int i = 0; i < productList.Length; i++)
+            {
+                string paddedProductCode = AddPadToNum(productList[i].Code);
+
+                Console.WriteLine("*Product Info *");
+                Console.WriteLine($"Product code: {paddedProductCode}");
+                Console.WriteLine($"Product codename: {productList[i].GetCodeName()}");
+                Console.WriteLine($"Product info: {productList[i].Description}");
+                Console.WriteLine($"Product base price: {productList[i].Price}€");
+                Console.WriteLine($"Product price + vat: {productList[i].GetPriceWithVat()}€");
+                Console.WriteLine($"Product Vat applied: {productList[i].VAT}%");
+                Console.WriteLine();
+            }
+
+
         }
     }
 }
